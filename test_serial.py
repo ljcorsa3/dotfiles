cc53f85759d27c446d6df50d1a6b4500ea688577
Larry Corsa <larry.corsa@drs.com>
2023-11-10 21:50:45 -0500

    auto

--- test_serial.py
+++ test_serial.py
@@ -10,7 +10,7 @@ import re
 port = '/dev/ttyUSB0'
 ser = serial.Serial(port)
 ser.baudrate = 115200
-ser.timeout = 0.5
+ser.timeout = 0.3
 
 try:
     ser.open()
@@ -31,8 +31,6 @@ def serCmd(cmd, check=True):
     global ser
     global verbose
 
-    esr = -1
-    esri = -1
     responses = [ 0, cmd ]
 
     # chase every command/query with *ESR?
@@ -58,32 +56,39 @@ def serCmd(cmd, check=True):
         resp = line.strip().split(';')
         for i in range(len(resp)):
             r = resp[i]
-            print(str(r).strip())
+            #print(str(r).strip())
             if str(r).strip().find('*ESR?') >= 0:
                 found=True
     # read lines until we see our expected response
+    esr = -1
+    esri = -1
     found = False
     while not found:
         line = ser.readline()
         if len(line) == 0:
             time.sleep(0.06)
             continue
-        print(f"line: {line}")
+        #print(f"line: {line}")
         line = re.sub('[,]?(\000|\r|\n)*$','',line.decode()).strip()
         resp = line.strip().split(';')
         for i in range(len(resp)):
             r = resp[i]
-            print(str(r).strip())
             if str(r).find('ESR ') >= 0:
                 esr = int(str(r)[4:])
                 esri = i
+            if esri < 0:
+                # trim string, clean up nulls, add to return val
+                s = re.sub(r'\000',r'\r\n',str(r).strip())
+                responses.append(s)
             if str(r).find(expected) >= 0:
                 found=True
     if esr>0:
         responses[0]=esr
     return responses
 
-n = serCmd('PWD DRS00015')
-n = serCmd('*idn?')
-n = serCmd('cfg1;#cop2; cfg 0')
+#n = serCmd('PWD DRS00015'); print(n)
+n = serCmd('*idn?'); print(n)
+n = serCmd('cfg1;#cop2; fid?; cfg 0'); print(n)
+n = serCmd('PWD password; cap?; *esr?'); print(n)
+n = serCmd('cfg1;#cop2; cfg 0'); print(n)
 
