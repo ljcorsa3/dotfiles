84aab0d9ced4590aae84a9149ae27b71d0942b89
Larry Corsa <larry.corsa@drs.com>
2024-02-07 21:42:10 -0500

    autobackup Wed Feb  7 09:42:10 PM EST 2024

--- bin/do-git-sync.sh
+++ bin/do-git-sync.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+if ! command cd ~/mnt/f/ljcorsa/sync; then
+    printf "f/ljcorsa/sync is not mounted!\n" >&2
+    exit 1
+fi
+
 readarray -t dirs < <(
-    find . -mindepth 1 -xdev -type d -name .git -printf '%P\0' |
+    find "${HOME}" -mindepth 1 -xdev -type d -name .git -printf '%P\0' |
     xargs -r0 -L 1 dirname
 )
-
-command cd ~/mnt/f/ljcorsa/sync
 
 for d in "${dirs[@]}"; do
     (
         mkdir -p "$d"
-        command cd $d; pwd
+        if ! command cd $d; then
+            printf "cannot cd to $PWD/$d" >&2
+            exit # this subshell
+        fi
+        pwd
         if [ -d .git ]; then
             git pull -v 
         else
